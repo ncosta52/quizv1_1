acda0a88e20ea806706f256f39863de2a415bc20
ncosta52 <ncosta52@sapo.pt>
2016-12-12 16:39:57 +0000

    teste sincronização 3

--- cmu_tp_1617/quizv1/app/src/main/java/com/tpv13/costa/nuno/quizv1/QuemQuerSerMilio.java
+++ cmu_tp_1617/quizv1/app/src/main/java/com/tpv13/costa/nuno/quizv1/QuemQuerSerMilio.java
@@ -11,5 +11,7 @@ public class QuemQuerSerMilio extends AppCompatActivity {
         setContentView(R.layout.activity_quem_quer_ser_milio);
 
         //teste
+        // testes
+        //    testestetss
     }
 }
