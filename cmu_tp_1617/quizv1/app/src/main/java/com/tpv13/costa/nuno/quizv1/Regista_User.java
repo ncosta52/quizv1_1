1ff90740def3bb90b1ef172e547e13ebbb5e8a67
gil2d <gil2d@hotmail.com>
2017-01-09 15:54:00 +0000

    Regista_user (só ideia, ainda nao funca, aceito sugestoes)

---
+++ cmu_tp_1617/quizv1/app/src/main/java/com/tpv13/costa/nuno/quizv1/Regista_User.java
@@ -0,0 +1,13 @@
+package com.tpv13.costa.nuno.quizv1;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Regista_User extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_regista__user);
+    }
+}
