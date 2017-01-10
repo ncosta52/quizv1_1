a64d0df06521bff18134a46f0624c32a31ba9a59
gil2d <gil2d@hotmail.com>
2017-01-10 15:27:57 +0000

    Regista_user (valida users e passwords)

--- cmu_tp_1617/quizv1/app/src/main/java/com/tpv13/costa/nuno/quizv1/Regista_User.java
+++ cmu_tp_1617/quizv1/app/src/main/java/com/tpv13/costa/nuno/quizv1/Regista_User.java
@@ -2,6 +2,8 @@ package com.tpv13.costa.nuno.quizv1;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.graphics.drawable.Drawable;
 import android.net.Uri;
 import android.provider.MediaStore;
@@ -9,22 +11,28 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 import java.io.File;
 import java.net.URI;
 
-public class Regista_User extends Activity implements View.OnClickListener
-{
+public class Regista_User extends Activity implements View.OnClickListener {
     Button bt_cam;
     ImageView imageView;
 
-    private Button bt_cancelar;
+    private Button bt_cancelar, bt_guardar;
+    private EditText edit_User, edit_Pass, edit_Confirm_Pass;
+
+    private MyDbHelper_game dbHelper;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_regista__user);
+
+        dbHelper = new MyDbHelper_game(this);
 
         bt_cam = (Button) findViewById(R.id.bt_uploadFoto);
         imageView = (ImageView) findViewById(R.id.image_view);
@@ -32,11 +40,17 @@ public class Regista_User extends Activity implements View.OnClickListener
 
         bt_cancelar = (Button) findViewById(R.id.bt_cancelar);
         bt_cancelar.setOnClickListener(this);
+
+        bt_guardar = (Button) findViewById(R.id.bt_guardar);
+        bt_guardar.setOnClickListener(this);
+
+        edit_User = (EditText) findViewById(R.id.ed_new_user);
+        edit_Pass = (EditText) findViewById(R.id.ed_new_pass);
+        edit_Confirm_Pass = (EditText) findViewById(R.id.ed_new_pass_confirm);
     }
 
     @Override
-    public void onClick(View view)
-    {
+    public void onClick(View view) {
 //        Intent camera_intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
 //        File file = getFile();
 //        camera_intent.putExtra(MediaStore.EXTRA_OUTPUT, Uri.fromFile(file));
@@ -49,30 +63,39 @@ public class Regista_User extends Activity implements View.OnClickListener
             case R.id.bt_cancelar:
                 finish();
                 break;
+            case R.id.bt_guardar:
+                if (userExist() == false)
+                {
+                    if(passValida() == true)
+                    {
+                        Toast.makeText(this, "Tudo valido", Toast.LENGTH_LONG).show();
+                        //GRAVAR BD
+                    }
+                    else
+                    {   Toast.makeText(this, "Reveja a password", Toast.LENGTH_LONG).show();}
+                }
+                break;
             default:
                 break;
         }
     }
 
-    private void chamaCamera()
-    {
+    private void chamaCamera() {
         Intent camera_intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
         File file = getFile();
         camera_intent.putExtra(MediaStore.EXTRA_OUTPUT, Uri.fromFile(file));
-        startActivityForResult(camera_intent,1);
+        startActivityForResult(camera_intent, 1);
     }
 
 
-    private File getFile()
-    {
+    private File getFile() {
         File folder = new File("sdcard/camera_app"); //sdcard/quizImages
 
-        if(!folder.exists())
-        {
+        if (!folder.exists()) {
             folder.mkdir();
         }
 
-        File image_file = new File(folder,"cam_image.jpg"); //username.jpg
+        File image_file = new File(folder, "cam_image.jpg"); //username.jpg
         return image_file;
     }
 
@@ -81,4 +104,49 @@ public class Regista_User extends Activity implements View.OnClickListener
         String path = "sdcard/camera_app/cam_image.jpg";
         imageView.setImageDrawable(Drawable.createFromPath(path));
     }
+
+    private boolean userExist() {
+        boolean existe = false;
+        Cursor d;
+        SQLiteDatabase db = dbHelper.getReadableDatabase();
+        String query;
+        query = "SELECT * FROM Utilizadores WHERE Username='" + this.edit_User.getText().toString().trim() + "'";
+
+        d = db.rawQuery(query, null);
+
+        if (d.getCount() > 0) {
+//
+            Toast.makeText(this, "Este utilizador já existe", Toast.LENGTH_LONG).show();
+            existe = true;
+        } else {
+            existe = false;
+        }
+
+        d.close();
+        return existe;
+    }
+
+    private boolean passValida()
+    {
+        boolean valido = false;
+
+        if(this.edit_Pass.getText().toString().trim().length() > 0)
+        {
+            if(this.edit_Pass.getText().toString().trim().equals(this.edit_Confirm_Pass.getText().toString().trim())) {
+                return true;
+            }
+            else
+            {
+                Toast.makeText(this, "As passwords não são iguais!", Toast.LENGTH_LONG).show();
+            }
+        }
+        else
+        {
+            Toast.makeText(this, "Insira uma password!", Toast.LENGTH_LONG).show();
+            return false;
+        }
+
+        return false;
+    }
+
 }
